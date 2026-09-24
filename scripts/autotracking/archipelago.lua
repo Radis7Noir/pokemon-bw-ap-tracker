b01d8986c43634ce0cde13e440d40d12ff00f8ee
pantin21 <trunks161@hotmail.fr>
2026-09-24 13:53:42 +0200

    all sanity locations on the dex maps, couple of fixes

--- scripts/autotracking/archipelago.lua
+++ scripts/autotracking/archipelago.lua
@@ -1,6 +1,5 @@
 ScriptHost:LoadScript("scripts/autotracking/item_mapping.lua")
 ScriptHost:LoadScript("scripts/autotracking/location_mapping.lua")
-ScriptHost:LoadScript("scripts/utils.lua")
 ScriptHost:LoadScript("scripts/autotracking/encounter_mapping.lua")
 ScriptHost:LoadScript("scripts/autotracking/flag_mapping.lua")
 ScriptHost:LoadScript("scripts/autotracking/map_mapping.lua")
